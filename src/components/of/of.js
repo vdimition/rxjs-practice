f5f6910553be41f706f72c3d1869b99a2b61d11e
vadimition <vadym.sirenko.triare@gmail.com>
2021-07-08 14:55:59 +0300

    fix: clear code

--- src/components/of/of.js
+++ src/components/of/of.js
@@ -4,7 +4,7 @@ import operators from 'rxjs/operators';
 const { of } = pkg;
 const { takeLast } = operators;
 
-const stream$ = of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).pipe(takeLast(5),);
+const stream$ = of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).pipe(takeLast(5));
 
 stream$.subscribe(
   (val) => console.log(val),
